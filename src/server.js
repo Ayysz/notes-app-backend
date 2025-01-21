c21e970909b57d1ab2028c6e02a1b1d6dee47f5d
Ayysz <ammar.xoc2@gmail.com>
2025-01-21 14:20:59 +0700

    fix: install dependency cross-env and fix the check production mode

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
-
 const init = async () => {
+  // eslint-disable-next-line no-undef
+  const isDevelopment = process.env.NODE_ENV !== 'production';
   const server = Hapi.Server({
     port: 5000,
-    host: 'localhost'
+    host: isDevelopment ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
   server.route(routes);
