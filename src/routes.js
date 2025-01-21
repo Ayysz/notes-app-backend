8a1afccd46be5f6da0a3620ba7007cfa0a20d667
Ayysz <ammar.xoc2@gmail.com>
2025-01-21 16:30:12 +0700

    feat: add editNoteByIdHandler and corresponding route for updating notes
    - Implemented editNoteByIdHandler in `src/handler.js` to handle PUT requests for updating notes.
    - Added a new route in `src/routes.js` to support the editing of notes by ID.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, deleteNoteByIdHandler, editNoteByIdHandler } = require('./handler');
 
 const routes = [
   {
@@ -20,6 +20,11 @@ const routes = [
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
   },
   {
     method: 'DELETE',
